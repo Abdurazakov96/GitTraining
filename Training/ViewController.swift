afe8d3521f0e6213349b8916d0883dd9ba50cc80
Muhammad <abdurazakov96@mail.ru>
2020-07-04 22:53:39 +0300

    Initial Commit

---
+++ Training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by Магомед Абдуразаков on 04.07.2020.
+//  Copyright © 2020 Магомед Абдуразаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

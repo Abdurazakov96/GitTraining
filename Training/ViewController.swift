fb08a4c4c9b45952465acf0ce67e54154a99c7c1
Muhammad <abdurazakov96@mail.ru>
2020-07-05 16:16:05 +0300

    Initial commit

--- Training/ViewController.swift
+++ Training/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  animate()
     }
 
-
+    func animate() {
+        UIView.animate(withDuration: 3, delay: 0, options: .curveEaseIn, animations: {
+            self.view.backgroundColor = UIColor.red
+        })
+        
+        label.backgroundColor = UIColor.green
+        label.frame = CGRect(x: 150, y: 10, width: 100, height: 300)
+//        label.bounds = CGRect(x: 1150, y: 30, width: 100, height: 100)
+        label.bounds = CGRect(x: 10, y: 10, width: 30, height: 30)
+        
+        label.text = "FDSFDSF"
+    }
+    
 }
 
